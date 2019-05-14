d214924d1fca1b13aa7d725138c7f14d2f1da31f
Yuliia Zelinska <yzelinska@lab.local>
2019-05-14 14:38:38 +0200

    Delegates. First look.

---
+++ Delegate_Value-ref_type/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Delegate_Value_ref_type
+{
+    using System;
+    delegate void StringProcessor(string input);
+    class Person
+    {
+        private readonly string name;
+        public Person(string name) { this.name = name; }
+        public void Say(string message)
+        {
+            Console.WriteLine("{0} says: {1}", name, message);
+        }
+    }
+    class Background
+    {
+        public static void Note(string note)
+        {
+            Console.WriteLine("({0})", note);
+        }
+    }
+    class SimpleDelegateUse
+    {
+        static void Main()
+        {
+            Person jon = new Person("Jon");
+            Person tom = new Person("Tom");
+            StringProcessor jonsVoice, tomsVoice, background;
+            jonsVoice = new StringProcessor(jon.Say);
+            tomsVoice = new StringProcessor(tom.Say);
+            background = new StringProcessor(Background.Note);
+            jonsVoice("Hello, son.");
+            tomsVoice.Invoke("Hello, Daddy!");
+            background("An airplane flies past.");
+        }
+    }
+}
